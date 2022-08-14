9bad1e5f270be41b64e5ba5232cdef7277442882
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-08-14 00:27:07 -0400

    changed options var from const to let

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,8 @@
-
-
 function getComputerChoice() {
-  const options = ["Rock", "Paper", "Scissors"];
+  let options = ["Rock", "Paper", "Scissors"];
   randNum = options.length;
-  choice = options[Math.floor(Math.random() * randNum)]
-  return choice
+  choice = options[Math.floor(Math.random() * randNum)];
+  return choice;
 }
 
 getComputerChoice();

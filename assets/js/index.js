3d5c36117eb5b48ddc7307abc38f4452b5f1ab77
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-09-13 11:14:19 -0400

    clicking a button will now play a round, a  bug has also been resolved when the users selects paper and the computer chooses scissors. it will no longer throw a spelling error.

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,7 @@ let options = ["rock", "paper", "scissors"];
 
 // The computer decision happens below
 function getComputerChoice() {
-  index = options.length
+  index = options.length;
   choice = options[Math.floor(Math.random() * index)];
   return choice;
 }
@@ -20,17 +20,17 @@ function playRound(playerSelection, computerSelection) {
   ) {
     let result = "Well done, you win!";
     wins++;
-    return result;
+    return `${result}\n ${wins} wins, ${loss} losses, ${tie} ties`;
   }
   // User Lose
   else if (
     (playerSelection === "rock" && computerSelection === "paper") ||
     (playerSelection === "scissors" && computerSelection === "rock") ||
-    (playerSelection === "paper" && computerSelection === "paper")
+    (playerSelection === "paper" && computerSelection === "scissors")
   ) {
     let result = "Sorry bud, maybe next time.";
     loss++;
-    return result;
+    return `${result}\n ${wins} wins, ${loss} losses, ${tie} ties`;
   }
   // User Tie
   else if (
@@ -40,7 +40,7 @@ function playRound(playerSelection, computerSelection) {
   ) {
     let result = "Folks we have got ourselves a tie!";
     tie++;
-    return result;
+    return `${result}\n ${wins} wins, ${loss} losses, ${tie} ties`;
   }
   // Error Check for spelling & input.
   else if (
@@ -55,45 +55,60 @@ function playRound(playerSelection, computerSelection) {
 }
 
 // RPS-UI
-const body = document.querySelector('body')
+const body = document.querySelector("body");
 
-const container = document.createElement('div')
-container.setAttribute('id', 'container')
+const container = document.createElement("div");
+container.setAttribute("id", "container");
 
-const content = document.createElement('div')
-content.setAttribute('id', 'content')
-content.setAttribute('style', 'display: flex; justify-content: center; gap: 2rem;')
+const content = document.createElement("div");
+content.setAttribute("id", "content");
+content.setAttribute(
+  "style",
+  "display: flex; justify-content: center; gap: 2rem;"
+);
 
+body.appendChild(container);
+container.appendChild(content);
 
-body.appendChild(container)
-container.appendChild(content)
+const rockButton = document.createElement("button");
+rockButton.setAttribute("id", "rockButton");
+rockButton.textContent = "Rock!";
+content.appendChild(rockButton);
 
+rockButton.addEventListener('click', () => {
+  playerSelection = 'rock'
+  getComputerChoice()
 
-
-const rockButton = document.createElement('button')
-rockButton.setAttribute('id', 'rockButton')
-rockButton.textContent = 'Rock!'
-content.appendChild(rockButton)
-
-const paperButton = document.createElement('button')
-paperButton.setAttribute('id', 'paperButton')
-paperButton.textContent = 'Paper!'
-content.appendChild(paperButton)
-
-const scissorsButton = document.createElement('button')
-scissorsButton.setAttribute('id', 'scissorsButton')
-scissorsButton.textContent = 'Scissors!'
-content.appendChild(scissorsButton)
-
-scissorsButton.addEventListener('click', () => {
-  alert('Scissors')
+  console.log(`The player has chosen ${playerSelection}, the computer has chosen ${choice}!`)
+  console.log(playRound(playerSelection, choice))
 })
 
+const paperButton = document.createElement("button");
+paperButton.setAttribute("id", "paperButton");
+paperButton.textContent = "Paper!";
+content.appendChild(paperButton);
 
+paperButton.addEventListener("click", () => {
+  playerSelection = "paper";
+  getComputerChoice();
 
+  console.log(
+    `The player has chosen ${playerSelection}, the computer has chosen ${choice}!`
+  );
+  console.log(playRound(playerSelection, choice));
+});
 
+const scissorsButton = document.createElement("button");
+scissorsButton.setAttribute("id", "scissorsButton");
+scissorsButton.textContent = "Scissors!";
+content.appendChild(scissorsButton);
 
+scissorsButton.addEventListener("click", () => {
+  playerSelection = "scissors";
+  getComputerChoice();
 
-
-
-
+  console.log(
+    `The player has chosen ${playerSelection}, the computer has chosen ${choice}!`
+  );
+  console.log(playRound(playerSelection, choice));
+});

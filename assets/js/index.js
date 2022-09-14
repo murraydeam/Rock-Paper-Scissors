973deb53320f4dfe25448140f616768e700d375e
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-09-14 12:04:21 -0400

    win, loss, and tie, conditions have been added. an alert message will now appear after the designated number of rounds have been played.

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,43 +25,44 @@ content.setAttribute(
 
 const scoreDisplays = document.createElement("div");
 scoreDisplays.setAttribute("id", "scoreDisplays");
-scoreDisplays.setAttribute("style", "display: flex; gap: 16px; justify-content: center; align-items: center;")
-
+scoreDisplays.setAttribute(
+  "style",
+  "display: flex; gap: 16px; justify-content: center; align-items: center;"
+);
 
 container.appendChild(scoreDisplays);
 
-
 const winDisplay = document.createElement("div");
-winDisplay.setAttribute("id", "winsDisplay")
-const winsTitle = document.createElement("h2")
-winsTitle.innerText = "Wins"
-const winsNum = document.createElement("h3")
-winsNum.setAttribute('id', 'winsNum')
+winDisplay.setAttribute("id", "winsDisplay");
+const winsTitle = document.createElement("h2");
+winsTitle.innerText = "Wins";
+const winsNum = document.createElement("h3");
+winsNum.setAttribute("id", "winsNum");
 winsNum.textContent = wins;
-winDisplay.appendChild(winsTitle)
-winDisplay.appendChild(winsNum)
+winDisplay.appendChild(winsTitle);
+winDisplay.appendChild(winsNum);
 
 const lossDisplay = document.createElement("div");
-lossDisplay.setAttribute("id", "lossDisplay")
-const lossTitle = document.createElement("h2")
-lossTitle.innerText = "Loss"
-const lossNum = document.createElement("h3")
-lossNum.setAttribute('id', 'lossNum')
+lossDisplay.setAttribute("id", "lossDisplay");
+const lossTitle = document.createElement("h2");
+lossTitle.innerText = "Loss";
+const lossNum = document.createElement("h3");
+lossNum.setAttribute("id", "lossNum");
 
 lossNum.textContent = loss;
-lossDisplay.appendChild(lossTitle)
-lossDisplay.appendChild(lossNum)
+lossDisplay.appendChild(lossTitle);
+lossDisplay.appendChild(lossNum);
 
 const tieDisplay = document.createElement("div");
-tieDisplay.setAttribute("id", "tieDisplay")
-const tieTitle = document.createElement("h2")
-tieTitle.innerText = "Ties"
-const tieNum = document.createElement("h3")
-tieNum.setAttribute('id', 'tieNum')
+tieDisplay.setAttribute("id", "tieDisplay");
+const tieTitle = document.createElement("h2");
+tieTitle.innerText = "Ties";
+const tieNum = document.createElement("h3");
+tieNum.setAttribute("id", "tieNum");
 
 tieNum.textContent = tie;
-tieDisplay.appendChild(tieTitle)
-tieDisplay.appendChild(tieNum)
+tieDisplay.appendChild(tieTitle);
+tieDisplay.appendChild(tieNum);
 
 scoreDisplays.appendChild(winDisplay);
 scoreDisplays.appendChild(lossDisplay);
@@ -83,7 +84,17 @@ function playRound(playerSelection, computerSelection) {
   ) {
     let result = "Well done, you win!";
     wins++;
-    document.getElementById('winsNum').innerText = wins
+    document.getElementById("winsNum").innerText = wins;
+    if (wins > 4) {
+      return (
+        alert(
+          "Great! You have beaten the computer! \n Click 'okay' to play again"
+        ),
+        (wins = 0),
+        (loss = 0),
+        (tie = 0)
+      );
+    }
     return `${result}\n ${wins} wins, ${loss} losses, ${tie} ties`;
   }
   // User Lose
@@ -94,7 +105,15 @@ function playRound(playerSelection, computerSelection) {
   ) {
     let result = "Sorry bud, maybe next time.";
     loss++;
-    document.getElementById('lossNum').innerText = loss
+    document.getElementById("lossNum").innerText = loss;
+    if (loss > 4) {
+      return (
+        alert("Maybe next time! \n Click 'okay' to play again"),
+        (wins = 0),
+        (loss = 0),
+        (tie = 0)
+      );
+    }
     return `${result}\n ${wins} wins, ${loss} losses, ${tie} ties`;
   }
   // User Tie
@@ -105,8 +124,17 @@ function playRound(playerSelection, computerSelection) {
   ) {
     let result = "Folks we have got ourselves a tie!";
     tie++;
-    document.getElementById('tieNum').innerText = tie
-
+    document.getElementById("tieNum").innerText = tie;
+    if (tie > 8) {
+      return (
+        alert(
+          "Try again looks like we have ourself a tie! \n Click 'okay' to play again"
+        ),
+        (wins = 0),
+        (loss = 0),
+        (tie = 0)
+      );
+    }
     return `${result}\n ${wins} wins, ${loss} losses, ${tie} ties`;
   }
   // Error Check for spelling & input.

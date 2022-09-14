4f61b350e5ba115ec5687701c35854d6fee8f657
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-09-14 11:50:14 -0400

    Wins, loss, & ties are all now displayed on the webpage using some simple styling and js DOM control.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,70 @@ let loss = 0;
 let tie = 0;
 let options = ["rock", "paper", "scissors"];
 
+// RPS-UI
+const body = document.querySelector("body");
+
+const container = document.createElement("div");
+container.setAttribute("id", "container");
+
+const content = document.createElement("div");
+content.setAttribute("id", "userButtons");
+content.setAttribute(
+  "style",
+  "display: flex; justify-content: center; gap: 2rem;"
+);
+
+body.appendChild(container);
+container.appendChild(content);
+content.setAttribute(
+  "style",
+  "display: flex; justify-content: center; gap: 2rem;"
+);
+
+const scoreDisplays = document.createElement("div");
+scoreDisplays.setAttribute("id", "scoreDisplays");
+scoreDisplays.setAttribute("style", "display: flex; gap: 16px; justify-content: center; align-items: center;")
+
+
+container.appendChild(scoreDisplays);
+
+
+const winDisplay = document.createElement("div");
+winDisplay.setAttribute("id", "winsDisplay")
+const winsTitle = document.createElement("h2")
+winsTitle.innerText = "Wins"
+const winsNum = document.createElement("h3")
+winsNum.setAttribute('id', 'winsNum')
+winsNum.textContent = wins;
+winDisplay.appendChild(winsTitle)
+winDisplay.appendChild(winsNum)
+
+const lossDisplay = document.createElement("div");
+lossDisplay.setAttribute("id", "lossDisplay")
+const lossTitle = document.createElement("h2")
+lossTitle.innerText = "Loss"
+const lossNum = document.createElement("h3")
+lossNum.setAttribute('id', 'lossNum')
+
+lossNum.textContent = loss;
+lossDisplay.appendChild(lossTitle)
+lossDisplay.appendChild(lossNum)
+
+const tieDisplay = document.createElement("div");
+tieDisplay.setAttribute("id", "tieDisplay")
+const tieTitle = document.createElement("h2")
+tieTitle.innerText = "Ties"
+const tieNum = document.createElement("h3")
+tieNum.setAttribute('id', 'tieNum')
+
+tieNum.textContent = tie;
+tieDisplay.appendChild(tieTitle)
+tieDisplay.appendChild(tieNum)
+
+scoreDisplays.appendChild(winDisplay);
+scoreDisplays.appendChild(lossDisplay);
+scoreDisplays.appendChild(tieDisplay);
+
 // The computer decision happens below
 function getComputerChoice() {
   index = options.length;
@@ -11,7 +75,6 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  userWins = 0;
   // User Winning
   if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
@@ -20,6 +83,7 @@ function playRound(playerSelection, computerSelection) {
   ) {
     let result = "Well done, you win!";
     wins++;
+    document.getElementById('winsNum').innerText = wins
     return `${result}\n ${wins} wins, ${loss} losses, ${tie} ties`;
   }
   // User Lose
@@ -30,6 +94,7 @@ function playRound(playerSelection, computerSelection) {
   ) {
     let result = "Sorry bud, maybe next time.";
     loss++;
+    document.getElementById('lossNum').innerText = loss
     return `${result}\n ${wins} wins, ${loss} losses, ${tie} ties`;
   }
   // User Tie
@@ -40,6 +105,8 @@ function playRound(playerSelection, computerSelection) {
   ) {
     let result = "Folks we have got ourselves a tie!";
     tie++;
+    document.getElementById('tieNum').innerText = tie
+
     return `${result}\n ${wins} wins, ${loss} losses, ${tie} ties`;
   }
   // Error Check for spelling & input.
@@ -48,40 +115,25 @@ function playRound(playerSelection, computerSelection) {
     playerSelection != "paper" ||
     playerSelection != "scissors"
   ) {
-    let result =
-      "Check you spelling, your must choose Rock, Paper or Scissors.";
-    return result;
+    let error = "Check you spelling, your must choose Rock, Paper or Scissors.";
+    return error;
   }
 }
-
-// RPS-UI
-const body = document.querySelector("body");
-
-const container = document.createElement("div");
-container.setAttribute("id", "container");
-
-const content = document.createElement("div");
-content.setAttribute("id", "content");
-content.setAttribute(
-  "style",
-  "display: flex; justify-content: center; gap: 2rem;"
-);
-
-body.appendChild(container);
-container.appendChild(content);
 
 const rockButton = document.createElement("button");
 rockButton.setAttribute("id", "rockButton");
 rockButton.textContent = "Rock!";
 content.appendChild(rockButton);
 
-rockButton.addEventListener('click', () => {
-  playerSelection = 'rock'
-  getComputerChoice()
+rockButton.addEventListener("click", () => {
+  playerSelection = "rock";
+  getComputerChoice();
 
-  console.log(`The player has chosen ${playerSelection}, the computer has chosen ${choice}!`)
-  console.log(playRound(playerSelection, choice))
-})
+  console.log(
+    `The player has chosen ${playerSelection}, the computer has chosen ${choice}!`
+  );
+  console.log(playRound(playerSelection, choice));
+});
 
 const paperButton = document.createElement("button");
 paperButton.setAttribute("id", "paperButton");
